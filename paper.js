8823d86105bb0ccc9b3281f71bc770889f6a2dcc
Kevin312007 <79404476+Kevin312007@users.noreply.github.com>
2021-06-19 21:23:54 +0800

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,30 @@
+class Paper 
+{
+
+    constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.paperWidth=60;
+		this.paperHeight=60;
+		this.body=Bodies.circle(x,y,60)
+		World.add(world,this.body)
+		
+		
+		this.image=loadImage("paper.png")
+    }
+	display(){
+		imageMode(CENTER)
+		image(this.image,this.body.position.x,this.body.position.y,this.paperWeight,this.paperHeight)
+	}
+
+
+
+
+
+
+
+
+
+
+}
